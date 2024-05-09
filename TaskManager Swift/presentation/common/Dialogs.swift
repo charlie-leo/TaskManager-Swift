ff04437065c0fb4faa44c06a829a68b7d8722827
Charles Raj <charles.raj412@gmail.com>
2024-05-09 18:21:23 -0400

    Initial commit

---
+++ TaskManager Swift/presentation/common/Dialogs.swift
@@ -0,0 +1,18 @@
+//
+//  Dialogs.swift
+//  TaskManager Swift
+//
+//  Created by charles raj on 09/05/24.
+//
+
+import SwiftUI
+
+struct Dialogs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Dialogs()
+}
